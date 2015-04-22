10d1ae26568420f6c1c188587dec83e1e4da151e
john <jjgithinji04@gmail.com>
2015-04-22 12:13:24 +0300

    Should be like first working version

---
+++ naididitbkup.py
@@ -0,0 +1,75 @@
+	import os
+import re
+from string import letters
+
+import jinja2
+import webapp2
+
+from google.appengine.ext import db
+
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+								autoescape = True)
+
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self,template, **params):
+		t= jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self,template,**kw):
+		self.write(self.render_str(template,**kw))
+
+#defines the datastore`s parent for organizations sake
+def blog_key(name = 'default'):
+	return db.Key.from_path('blogs',name)		
+
+class Blog(db.Model):
+	title=db.StringProperty(required = True)
+	content =db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+	#sets property of when last modified	
+	last_modified = db.DateTimeProperty(auto_now = True)
+
+	
+
+
+class MainPage(Handler):
+
+	def render_naididit(self,blgtitle="",blgcontent="",error=""):
+		blog = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC")
+
+		self.render("naididit.html",blgtitle=blgtitle,blgcontent=blgcontent,error=error,blog=blog)
+		
+
+	def get(self):	 
+		self.render_naididit()
+		#self.response.headers['Content-Type'] = 'text/plain'
+		#self.render("naididit.html")
+			
+
+	def post(self):
+		title=self.request.get("blgtitle")
+		content= self.request.get("blgcontent")
+		#di=makeindex(title,content)
+
+		#self.render_naididit(content,title)
+		#self.response.headers['Content-Type'] = 'text/plain'
+		#self.write(self.request)
+
+		if title and content:
+
+			a = Blog(title = title,content = content)
+			a.put()
+
+			self.redirect('/)
+			
+		else:
+			error = "Got some rendering error"
+			self.render_naididit(content,title,error)
+
+app=webapp2.WSGIApplication([('/',MainPage),],debug = True)				
