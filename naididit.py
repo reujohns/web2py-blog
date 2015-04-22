956d7dd292ffa50b85e9744206b7110465954360
john <jjgithinji04@gmail.com>
2015-04-22 16:15:23 +0300

    Added user registration/signup.working.also corrected '/newpost' now working

--- naididit.py
+++ naididit.py
@@ -1,5 +1,8 @@
 import os
 import re
+import random
+import hashlib
+import hmac
 from string import letters
 
 import jinja2
@@ -16,10 +19,21 @@ def render_str(template, **params):
 		t= jinja_env.get_template(template)
 		return t.render(params)
 
-def render(self,template,**kw):
-		self.write(self.render_str(template,**kw))
+#def render(self,template,**kw):
+		#self.write(self.render_str(template,**kw))
 
-				
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+	return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")	
+def valid_password(password):	
+	return password and PASS_RE.match(password)
+
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+	return not email or EMAIL_RE.match(email)
+
 
 class BlogHandler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
@@ -31,6 +45,48 @@ class BlogHandler(webapp2.RequestHandler):
 
 	def render(self,template,**kw):
 		self.write(self.render_str(template,**kw))
+
+
+
+
+
+class Signup(BlogHandler):
+	def get(self):
+		self.render("userregistration.html")	
+
+	def post(self): 
+		have_error = False
+		username = 	self.request.get('username')
+		password = self.request.get('password')
+		verify = self.request.get('verify')
+		email = self.request.get('email')
+
+		params = dict(username = username, email= email)
+
+
+		if not valid_username(username):
+			params['error_username']= "That`s not a valid usersname."
+			have_error=True
+
+		if not	valid_password(password):
+			params['error_password'] = "That`s not a valid password."
+			have_error = True
+		elif password != verify:
+			params['error_verify'] = "Your password didn`t match."
+			have_error = True	
+
+		if not valid_email(email):
+			params['error_mail'] = "That`s not a valid email."
+			have_error = True
+		if have_error:
+			self.render('userregistration.html', **params)
+		else:
+			self.redirect('/welcome?username='+username)			
+
+
+
+
+
 
 #defines the datastore`s parent for organizations sake
 def blog_key(name = 'default'):
@@ -68,7 +124,7 @@ class BlogPost(BlogHandler):
 		
 class NewPost(BlogHandler):
 	def get(self):
-		self.render("newpost.html")
+		self.render('newpost.html')
 
 	def post(self):
 		subject=self.request.get('subject')
@@ -80,7 +136,7 @@ class NewPost(BlogHandler):
 			x=str(p.key().id())
 			#self.write('/blog / %s' % x)
 			#self.redirect('/blog / %s' % x )
-			self.redirect("/blog/%s"%x)
+			self.redirect('/blog/%s'%x)
 			#self.redirect(r'/blog/(\d+)>',PostHandler)
 		else:
 			error = "subject and content,please !"
@@ -110,8 +166,8 @@ class MainPage(BlogHandler):
 		#self.response.headers['Content-Type'] = 'text/plain'
 		self.render("newpost.html")
 
-	def post(self):
-		NewPost(BlogHandler)
+#	def post(self):
+#		NewPost(BlogHandler)
 
 #		subject=self.request.get('subject')
 #		content= self.request.get('content')
@@ -150,7 +206,8 @@ class MainPage(BlogHandler):
 app=webapp2.WSGIApplication([('/',MainPage),
 							  ('/blog/?',BlogFront),
 							  ('/blog/([0-9]+)',BlogPost),
-							  ('/blog/[0-9]+)',PostHandler),
+							  #('/blog/[0-9]+)',PostHandler),
 							  ('/blog/newpost',NewPost),
+							  ('/userregistration',Signup),
 							  ],
 							  debug = True)				
